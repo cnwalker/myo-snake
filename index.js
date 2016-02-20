f37f819b7f73ccf426873ea483a7513c1d6ddc9f
Henry <hlewis@uchicago.edu>
2016-02-20 15:56:03 -0600

    sending image to server

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+app.use( bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(bodyParser.raw({     // to support URL-encoded bodies
+    limit: '5MB',
+    type: 'application/octet-stream'
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,8 +23,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/image', function(request, response) {
+    console.log(request.body)
+    response.send('cool');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
