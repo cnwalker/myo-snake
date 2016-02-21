a54062ee1fa2d755f870aea89883d8b25a8f1bf7
Joseph Mittelstaedt <jmittelstaedt@uchicago.edu>
2016-02-20 23:34:26 -0600

    fixing some problems with json and finding the blobs2

--- python-script/cam_edge.py
+++ python-script/cam_edge.py
@@ -1,14 +1,8 @@
 import frontier
 import numpy as np
+##import matplotlib.pyplot as plt
 import cv2
 import json
-
-def take_pic():
-    cam = cv2.VideoCapture(1) # 1 is the connected webcam, IDK how to handle this... maybe better to have some web code which does this and just hands the image to python somehow.
-    s, frame = cam.read()
-    img_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cam.release()
-    return img_g
 
 def make_board(img,conv_x,conv_y):
     im_shape = img.shape
@@ -22,19 +16,37 @@ def make_board(img,conv_x,conv_y):
                 snake_board[i,j] = 1
     return snake_board
 
-def main():
-    img_g = take_pic()
+def get_canvas(img_g):
+
     edges = cv2.Canny(img_g,10,100,2)
     snake_map = make_board(edges,7,7)
-    snake_list = [list(row)for row in snake_map]
-    blob = front.get_big_blob(snake_map)
-    edge_array = snake_map.nonzero()
-    edge_pt_list = zip(edge_array[0],edge_array[1])
+
+    return snake_map
+
+
+
+def main():
+##    data = sys.stdin.read()
+##    #create file
+    fname = 'tmp/' + str(time.time()) + '.png'
+##    f=open (fname, 'wb')
+##    f.write(data)
+##    f.close()
+
+    #remove files
+##    os.remove('./' + fname)
+    img = sc2.imread(fname)
+    s_map = get_canvas(img)
+    blob = frontier.get_big_blob(s_map)
+    snake_list = s_map.tolist()
+    edge_array = s_map.nonzero()
+    edge_pt_list = zip(edge_array[0].tolist(),edge_array[1].tolist())
     for pt in blob:
-        snake_map[pt[0],pt[1]] = 1
+        s_map[pt[0],pt[1]] = 1
     bad_array = 1 - snake_map
     bad_array_indices = bad_array.nonzero()
-    bad_pt_list = zip(bad_array_indices[0],bad_array_indices[1])
+    bad_pt_list = zip(bad_array_indices[0].tolist(),bad_array_indices[1].tolist())
+    print type(bad_pt_list[2][1])
     result_dict = {'map_array' : snake_list, 'good_points' : blob, 'bad_points' : bad_pt_list, 'edge_list' : edge_pt_list}
     out_str = json.dumps(result_dict)
     print out_str
